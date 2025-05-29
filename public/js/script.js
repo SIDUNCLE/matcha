2b95b6fdaca94ec94d8f7efea8e73b83bc3899ef
SIDDHARTH <dceengineer2005@gmail.com>
2025-05-30 02:30:45 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,51 @@
 $(document).ready(function () {
-	$('.upload-btn').on('click', function () {
-		$('#upload-input').click();
-	});
-	$('#upload-input').on('change', function () {
-		var uploadInput = $('#upload-input');
-		if (uploadInput.val() != '') {
-			var formData = new FormData();
-			formData.append('upload', uploadInput[0].files[0]);
+  $('.upload-btn').on('click', function () {
+    $('#upload-input').click();
+  });
 
-			$.ajax({
-				url: '/uploadFile',
-				type: 'POST',
-				data: formData,
-				processData: false,
-				contentType: false,
-				success: function () {
-					uploadInput.val('');
-				}
-			});
-		}
-	});
+  $('#upload-input').on('change', function () {
+    var uploadInput = $('#upload-input');
+    if (uploadInput.val() != '') {
+      var formData = new FormData();
+      formData.append('upload', uploadInput[0].files[0]);
+
+      // Show progress bar
+      $('#upload-progress-container').show();
+      $('#upload-progress').val(0);
+      $('#upload-progress-text').text('0%');
+
+      $.ajax({
+        url: '/uploadFile',
+        type: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        xhr: function () {
+          var xhr = new window.XMLHttpRequest();
+          xhr.upload.addEventListener("progress", function (evt) {
+            if (evt.lengthComputable) {
+              var percentComplete = Math.round((evt.loaded / evt.total) * 100);
+              $('#upload-progress').val(percentComplete);
+              $('#upload-progress-text').text(percentComplete + '%');
+            }
+          }, false);
+          return xhr;
+        },
+        success: function () {
+          uploadInput.val('');
+          $('#upload-progress-container').hide();
+        },
+        error: function () {
+          $('#upload-progress-text').text('Error!');
+        }
+      });
+    }
+  });
 });
 
-// make chatroom auotoscroll
+// make chatroom autoscroll
 $(document).ready(function () {
-	$('#messages').animate({
-		scrollTop: 100000
-	}, 800);
+  $('#messages').animate({
+    scrollTop: 100000
+  }, 800);
 });
